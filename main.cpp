e82562eadce21ae3bbd090a14b7c2451abec0738
Adithya Somasundaram <adsomasu@ucsc.edu>
2020-07-27 18:19:21 -0700

    selection sort v1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include "sorting.hpp"
 
 #include <iostream>
-#include <array>
 #include <vector>
 #include <algorithm>
 #include <iterator>
 #include <sstream>
 
+using namespace std;
+
 int main(int argc, char* argv[]){
-    std::vector<double> arr(argc-1);
+    vector<double> arr(argc-1);
     for(int i = 1; i < argc; i++){
-        std::stringstream(argv[i]) >> arr[i-1];
+        stringstream(argv[i]) >> arr[i-1];
     }
     // std::copy(std::begin(og),std::end(og),arr.begin());
     selectionSort(arr, argc-1);
--- sorting.cpp
+++ sorting.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <vector>
 
-// using namespace std;
+using namespace std;
 
-void selectionSort(std::vector<double> arr, int len){
+void print(vector<double> in, int len);
+void swap(vector<double> &in, int a, int b);
 
-    for(int i = 0; i < len; i++){
-        std::cout << arr[i];
+void selectionSort(std::vector<double> arr, int len)
+{
+    cout << "*** SELECTION SORT ***" << endl;
+    cout << "Inputted array: ";
+    print(arr, len);
+
+    int j, min;
+
+    for (int i = 0; i < len; i++)
+    {
+        min = i;
+        for (j = i; j < len; j++){
+            if (arr[min] > arr[j]) min = j;
+        }
+        cout << i << ": ";
+        if (min != i) {
+            swap(arr, i, min);
+            print(arr, len);
+        } else {
+            cout << "Element at index " << i << " is less than elements to right of it -> no changes made" << endl;
+        }
     }
+}
+
+void print(vector<double> in, int len)
+{
+    for (int a = 0; a < len; a++)
+    {
+        cout << in[a] << " ";
+    }
+    cout << endl;
+}
+
+void swap(vector<double> &in, int a, int b)
+{
+    double temp = in[a];
+    in[a] = in[b];
+    in[b] = temp;
 }
